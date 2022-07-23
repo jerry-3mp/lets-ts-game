8ac00f54ece947b9df40f01b113cd1affdbd1226
skylee510 <skylee510@gmail.com>
2022-07-23 19:14:23 +0800

    initialize react-three-fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyCanvas from './MyCanvas';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyCanvas />
       </header>
     </div>
   );
