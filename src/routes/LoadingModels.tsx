316d6577eff77fe64c3e053945f5bd0793f0389d
jerry-3mp <jerry.3mp@gmail.com>
2022-07-31 16:17:41 +0800

    feat: loading pistol

--- src/routes/LoadingModels.tsx
+++ src/routes/LoadingModels.tsx
@@ -1,16 +1,24 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { Pistol } from "../components/Pistol";
 import { Poimandres } from "../components/Poimandres";
 
 function LoadingModels() {
     return (
         <div className = "App-header">
-            <Canvas style={{width: "300px", height: "300px"}}>
+            <Canvas style={{width: "80vw", height: "450px"}}>
                 <Suspense fallback={null}>
                     <Poimandres />
                     <OrbitControls />
                     <Environment preset="sunset" background />
+                </Suspense>
+            </Canvas>
+            <Canvas style={{width: "80vw", height: "450px"}}>
+                <Suspense fallback={null}>
+                    <Pistol />
+                    <OrbitControls />
+                    <Environment preset="lobby" background />
                 </Suspense>
             </Canvas>
         </div>
