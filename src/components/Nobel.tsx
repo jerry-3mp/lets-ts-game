fa9a9b6d1dc48a6db5f740b6674277aba0fe0da9
jerry-3mp <jerry.3mp@gmail.com>
2022-08-14 18:15:48 +0800

    fix: material and textures

--- src/components/Nobel.tsx
+++ src/components/Nobel.tsx
@@ -1,14 +1,44 @@
-// import { useFBX } from '@react-three/drei';
-import {useLoader} from '@react-three/fiber';
-import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+import {useFBX, useTexture} from '@react-three/drei';
+import {Mesh, MeshPhongMaterial, MeshStandardMaterial} from 'three';
 
 function Nobel() {
-  const fbx = useLoader(FBXLoader, '/models/nobel/nobel.fbx');
-  // const fbx = useFBX('/Poimandres.fbx');
-  return <primitive object={fbx}>
-    <ambientLight />
-    <pointLight position={[0, 0, 0]} />
-  </primitive>;
+  const fbx = useFBX('/models/nobel/nobel.fbx');
+  console.log((fbx.children[0] as Mesh).material);
+  const [aoMap, map, normalMap, roughnessMap, metalnessMap] =
+  useTexture( [
+    '/models/nobel/nobel_AOmixed.png',
+    '/models/nobel/nobel_Color.png',
+    '/models/nobel/nobel_Normal.png',
+    '/models/nobel/nobel_Roughness.png',
+    '/models/nobel/nobel_Metallic.png',
+  ]);
+  fbx.traverse((child)=>{
+    if (child instanceof Mesh) {
+      const oldMaterial = ((child as Mesh).material as MeshPhongMaterial);
+      child.material = new MeshStandardMaterial({
+        color: oldMaterial.color,
+        metalness: 2.5,
+        roughness: 3,
+        aoMap,
+        map,
+        normalMap,
+        roughnessMap,
+        metalnessMap,
+      });
+    }
+  });
+  console.log((fbx.children[0] as Mesh).material);
+  return <mesh>
+    <primitive object={fbx}>
+      <ambientLight />
+      <pointLight position={[100, 0, 0]} />
+      <pointLight position={[0, 100, 0]} />
+      <pointLight position={[0, 0, 100]} />
+      <pointLight position={[-100, 0, 0]} />
+      <pointLight position={[0, -100, 0]} />
+      <pointLight position={[0, 0, -100]} />
+    </primitive>
+  </mesh>;
 }
 
 export default Nobel;
