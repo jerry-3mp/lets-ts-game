93b456e6b9fd3739d02a1db92ac5b85be81489f6
jerry-3mp <109559135+jerry-3mp@users.noreply.github.com>
2022-11-13 12:06:57 +0800

    feat: implement tpv-movent page (#9)
    * feat: initialize tpv-movemet page
    
    * feat: implement tpv-movent page

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import GettingStarted from './routes/GettingStarted';
 import LoadingModels from './routes/LoadingModels';
 import BasicWorld from './routes/BasicWorld';
 import FPVMovement from './routes/FPVMovement';
+import TPVMovement from './routes/TPVMovement';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -21,6 +22,7 @@ root.render(
           <Route path="loading-models" element={<LoadingModels />} />
           <Route path="basic-world" element={<BasicWorld />} />
           <Route path="fpv-movement" element={<FPVMovement />} />
+          <Route path="tpv-movement" element={<TPVMovement />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>,
